a3dba5d9c230c64150f7c92b71162a67da8c84e4
Eewei Seah <seaheewei@gmail.com>
2023-02-18 14:01:33 +0800

    seeded

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,22 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+
+require "json"
+require "open-uri"
+
+url = "https://tmdb.lewagon.com/movie/top_rated"
+movies_serialized = URI.open(url).read
+movies = JSON.parse(movies_serialized)["results"]
+
+movies.each do |movie|
+  Movie.create!(
+    {
+      title: movie["title"],
+      overview: movie["overview"],
+      poster_url: "https://image.tmdb.org/t/p/w500#{movie["poster_path"]}",
+      rating: movie["vote_average"]
+    }
+  )
+end
