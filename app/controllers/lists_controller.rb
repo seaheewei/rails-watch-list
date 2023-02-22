e364b8264d0c5a36906e0fd229a9319e7b756ed6
Eewei Seah <seaheewei@gmail.com>
2023-02-22 19:28:18 +0800

    added delete function to lists

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -31,6 +31,12 @@ class ListsController < ApplicationController
     @movies = Movie.all
   end
 
+  def destroy
+    @list = List.find(params[:id])
+    @list.destroy
+    redirect_to lists_path, status: :see_other
+  end
+
   private
 
   def list_params
--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
-  resources :lists, only: [ :index, :new, :show, :create ] do
+  resources :lists, only: [ :index, :new, :show, :create, :destroy ] do
     resources :bookmarks, only: [:create]
   end
   resources :bookmarks, only: [:destroy]
